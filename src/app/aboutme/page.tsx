0d31c996abeee109905308c4d89816e9f905961d
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2024-06-03 06:25:27 +0400

    added qpous

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -7,14 +7,14 @@ const AboutPage = () => {
 
       <div className="mt-7 sm:mt-10 md:mt-14">
         <p className="text-sm sm:text-base md:text-lg ">
-          Hello! I&apos;m Zeshan, a Computer Science graduate with a passion for web development. I have project experience in frontend development, including working with React.js, HTML, CSS, and JavaScript. Additionally, I have a basic understanding of various frontend technologies and frameworks.
-        </p>
-        <p className="text-sm sm:text-base md:text-lg ">
-          As a newcomer in the field, my primary interest is in showcasing my skills and tackling real-world problems rather than just securing a job. I&apos;m eager to learn and adapt to new technologies, and I possess strong problem-solving skills and attention to detail. I believe Hyperspace is the perfect place for me to achieve this. I&apos;m excited to meet the team and connect personally to share more about myself.
-        </p>
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+          Hey there! I&apos;m Zeshan, a Computer Science graduate deeply passionate about web development. I've gained valuable project experience in frontend development, working extensively with React.js, HTML, CSS, and JavaScript. Additionally, I have a solid grasp of various frontend technologies and frameworks.
+
+          As someone new to the field, I&apos;m eagerly seeking opportunities to showcase my skills and solve real-world problems. I&apos;m known for my ability to quickly grasp new concepts and adapt to emerging technologies. My strengths lie in my strong problem-solving abilities and being a quick learner.
+
+          I see Hyperspace as the ideal platform to grow and thrive in my career. I&apos;m looking forward to connecting with the team and sharing more about my journey.
           Thanks!
         </p>
+
       </div>
     </>
   );
