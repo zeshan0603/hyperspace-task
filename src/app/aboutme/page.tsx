0d37ada7ec796c133e0da0674bb2782b2f601f1a
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2024-06-03 05:20:28 +0400

    fixed issues

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,27 +1,23 @@
-import gsap from "gsap"
-import { title } from "process";
+import gsap from "gsap";
 
 const AboutPage = () => {
-
-
-
-
   return (
     <>
       <h1 className="text-7xl font-extrabold text-center m-14">About me</h1>
 
       <div className="mt-10">
         <p>
-
-          Hello! I'm Zeshan, a Computer Science graduate with a passion for web development. I have project experience in frontend development, including working with React.js, HTML, CSS, and JavaScript. Additionally, I have a basic understanding of various frontend technologies and frameworks.
-
-          As a newcomer in the field, my primary interest is in showcasing my skills and tackling real-world problems rather than just securing a job. I'm eager to learn and adapt to new technologies, and I possess strong problem-solving skills and attention to detail. I believe Hyperspace is the perfect place for me to achieve this. I'm excited to meet the team and connect personally to share more about myself.
-
+          Hello! I&apos;m Zeshan, a Computer Science graduate with a passion for web development. I have project experience in frontend development, including working with React.js, HTML, CSS, and JavaScript. Additionally, I have a basic understanding of various frontend technologies and frameworks.
+        </p>
+        <p>
+          As a newcomer in the field, my primary interest is in showcasing my skills and tackling real-world problems rather than just securing a job. I&apos;m eager to learn and adapt to new technologies, and I possess strong problem-solving skills and attention to detail. I believe Hyperspace is the perfect place for me to achieve this. I&apos;m excited to meet the team and connect personally to share more about myself.
+        </p>
+        <p>
           Thanks!
         </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
