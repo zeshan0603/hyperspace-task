2af609634eea21cf2d34251c72fb2a114b7a41ff
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2024-06-03 05:24:57 +0400

    Fixed issues

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Intro() {
     return (
         <div>
+            <h1 className='text-7xl font-bold m-10 text-center'>Hi there!</h1>
             <p>
                 Thanks for giving me the chance to complete this test. I designed the Modal as a server component and incorporated a straightforward GSAP animation for the page transitions. I primarily utilized Tailwind CSS for styling. I hope you find it satisfactory!
             </p>
