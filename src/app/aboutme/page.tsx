90d3791db06d1f2bdd8af31d4d95e3eb9298ed64
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2024-06-03 06:27:26 +0400

    added qpous

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
 
       <div className="mt-7 sm:mt-10 md:mt-14">
         <p className="text-sm sm:text-base md:text-lg ">
-          Hey there! I&apos;m Zeshan, a Computer Science graduate deeply passionate about web development. I've gained valuable project experience in frontend development, working extensively with React.js, HTML, CSS, and JavaScript. Additionally, I have a solid grasp of various frontend technologies and frameworks.
+          Hey there! I&apos;m Zeshan, a Computer Science graduate deeply passionate about web development. I&apos; ve gained valuable project experience in frontend development, working extensively with React.js, HTML, CSS, and JavaScript. Additionally, I have a solid grasp of various frontend technologies and frameworks.
 
           As someone new to the field, I&apos;m eagerly seeking opportunities to showcase my skills and solve real-world problems. I&apos;m known for my ability to quickly grasp new concepts and adapt to emerging technologies. My strengths lie in my strong problem-solving abilities and being a quick learner.
 
